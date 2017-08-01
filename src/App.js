3b01ec462026428c280707070bcd63ac0eae1ec5
Adriano Interaminense <adriano.interaminense@gmail.com>
2017-08-01 00:47:40 -0300

    update demos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import Portfolio from './components/Portfolio/Portfolio';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
 
+    const URL_DEFAULT = '/react-portfolio-behance';
+
     const Param = ({ match }) => <Portfolio user={match.params.username} showFeatured />;
     const ParamDefault = ({ match }) => {
-      let listUsers = ['AitorPrieto', 'emas', 'pixego', 'akirax', 'dennisschafer', 'superfiction'];
+      let listUsers = ['AitorPrieto', 'atipo', 'Ramotion', 'akirax', 'dennisschafer', 'superfiction'];
       let username = listUsers[Math.floor(Math.random() * listUsers.length)];
 
       return <Portfolio user={username} showFeatured />;
     };
 
     return (
-      <Router>
-        <Switch>
-          <Route path="/react-portfolio-behance/:username" component={Param} />
-          <Route path="/react-portfolio-behance" component={ParamDefault} />
-        </Switch>
-      </Router>
+      <div>
+        <Router>
+          <Switch>
+            <Route path={`${URL_DEFAULT}/:username`} component={Param} />
+            <Route path={`${URL_DEFAULT}`} component={ParamDefault} />
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
     );
   }
 }
