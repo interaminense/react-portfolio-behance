f6758c5db58175a4682decc7db4c8984312f9334
Jessuir Cleydson <jessuircleydson@gmail.com>
2017-11-05 22:41:34 -0300

    Adjusting footer break point and grid

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,35 +6,33 @@ class Footer extends Component {
   render() {
     return (
       <footer className="Footer">
-        <div>
           <img src={LogoFooter} alt="footer portfolio + react + behance" />
-          <br />
-          <br />
-          <br />
-          Copyright © 2017. <i>Made with ❤ by Adriano Interaminense</i>
-        </div>
-        <ul>
-          <li>
-            <a href="https://github.com/interaminense/react-portfolio-behance"
-              rel="noopener noreferrer"
-              target="_blank" className="btn btn--default">React Portfolio Behance</a>
-          </li>
-          <li>
-            <a href="https://github.com/interaminense"
-              rel="noopener noreferrer"
-              target="_blank" className="btn btn--default">my github</a>
-          </li>
-          <li>
-            <a href="https://twitter.com/adinteraminense"
-              rel="noopener noreferrer"
-              target="_blank" className="btn btn--default">my twitter</a>
-          </li>
-          <li>
-            <a href="https://codepen.io/interaminense"
-              rel="noopener noreferrer"
-              target="_blank" className="btn btn--default">my codepen</a>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <a href="https://github.com/interaminense/react-portfolio-behance"
+                rel="noopener noreferrer"
+                target="_blank" className="btn btn--default">React Portfolio Behance</a>
+            </li>
+            <li>
+              <a href="https://github.com/interaminense"
+                rel="noopener noreferrer"
+                target="_blank" className="btn btn--default">my github</a>
+            </li>
+            <li>
+              <a href="https://twitter.com/adinteraminense"
+                rel="noopener noreferrer"
+                target="_blank" className="btn btn--default">my twitter</a>
+            </li>
+            <li>
+              <a href="https://codepen.io/interaminense"
+                rel="noopener noreferrer"
+                target="_blank" className="btn btn--default">my codepen</a>
+            </li>
+          </ul>
+
+        <p>
+           Copyright © 2017. <i>Made with ❤ by Adriano Interaminense</i>
+        </p>
       </footer>
     );
   }
